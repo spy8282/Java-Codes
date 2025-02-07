2dc615289ebf918a44dbbef80243239966e0a023
spy8282 <126975483+spy8282@users.noreply.github.com>
2025-02-07 11:42:55 +0530

    Add files via upload

---
+++ AddAll.java
@@ -0,0 +1,32 @@
+class AddAll 
+{
+	public static void main(String[] args) 
+	{
+		int num = 1234;
+		int sum = 0;
+		int rem = 0;
+		
+		rem = num%10;
+		sum = sum+rem;
+		num = num/10;
+		
+			
+		rem = num%10;
+		sum = sum+rem;
+		num = num/10;
+		
+		rem = num%10;
+		sum = sum+rem;
+		num = num/10;
+		
+		rem = num%10;
+		sum = sum+rem;
+		
+		System.out.println(sum);
+		
+		
+		
+		
+	
+	}
+}
